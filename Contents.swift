54512494a125db31c95cdb03332adb727a45e3d3
GallupinGallup <gallupingallup@gmail.com>
2016-09-15 13:09:01 -0600

    Created Strings

---
+++ Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var hello = "Hi"
+hello = " Hello!"
+var name = "My name is Jarvis"
+print(hello)
+print(name)
+var hellon = "\(hello) \(name)"
+print(hellon)
+var stringCount = hellon.characters.count
+print("this is how many charcters are in the line above: \(stringCount)")
